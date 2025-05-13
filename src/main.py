d78a71aaf9958c67a0b9df189057691ac1967204
urbnauq <eduardoalmonte18@gmai.com>
2025-05-13 04:43:24 -0400

    created text node file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
+from textnode import TextNode, TextType
+
 def main():
-    pass
+
+    text_node_0 = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(text_node_0)
 
 if __name__ == "__main__":
     main()
