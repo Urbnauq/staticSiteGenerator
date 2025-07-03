5ba7ce9c80fea170123d5c569f782c0078d1b107
urbnauq <eduardoalmonte18@gmai.com>
2025-07-02 20:07:16 -0400

    CH5:L5

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ from functions import copy_directories, generate_page_recursive
 import sys
 
 def main():
-
+    base_path = sys.argv[1]
+    
     source = "static"
     target = "docs"
     copy_directories(source, target)
@@ -10,7 +11,7 @@ def main():
     from_path = "content"
     template_path = "template.html"
     dest_path = "docs"
-    generate_page_recursive(from_path, template_path, dest_path)
+    generate_page_recursive(base_path, from_path, template_path, dest_path)
 
 if __name__ == "__main__":
     main()
